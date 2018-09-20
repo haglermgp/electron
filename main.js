cd3d1b01a231b0aa9caa6d1c7f5134844db5d081
hagler.mijail <hagler.mijail@gmail.com>
2018-09-20 09:22:17 -0500

    init config menu

--- main.js
+++ main.js
@@ -1,25 +1,91 @@
-// In this file we put the all code of our logic application
-const loader = require('monaco-loader');
-// this module that able us receive the event 'open-file' - check the indexjs
-const { ipcRenderer } = require('electron');
-const fs = require('fs');
+const { app, BrowserWindow, Menu, shell } = require('electron')
+const path = require('path')
+const url = require('url')
+// keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the Javascript object is garbage collected
 
-loader().then( monaco => {
-	let editor = monaco.editor.create(
-		document.querySelector('#app'),
-		{
-			language: 'javascript',
-			theme: 'vs-dark',
-			automaticLayout: true
-		}
-	)
+let win
 
-	ipcRenderer.on('open-file', (e, url) => {
-		fs.readFile(url.slice(10), 'utf-8', (err, data) => {
-			console.log('data >>>>', data)
-			console.log('url >>>>', url)
-			console.log('err >>>>', err)
-			editor.setModel(monaco.editor.createModel(data, 'javascript'));
-		})
+function createWindow (argument) {
+	// create the browser window
+	win = new BrowserWindow({ width: 800, height: 600 })
+
+	// and load the index.html of the app
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, 'src/index.html'),
+		protocol: 'file',
+		slashes: true
+	}))
+	win.loadFile('src/index.html')
+
+	// Open DevTools
+	win.webContents.openDevTools()
+
+	// Emitted when a window is closed 
+
+	win.on('closed', () => {
+		// Defference the window object, usually you should store window 
+		// in arrays if your app supports multi windows, this is the time 
+		// when you should delete the corresponding element
+
+		win = null
 	})
+
+	var menu = Menu.buildFromTemplate([
+		{
+			label: 'Menu',
+			submenu: [
+				{ label: 'Adjust Notification Value' },
+				{ 
+					label: 'CoinMarketCap',
+					click() {
+						shell.openExternal('https://www.coinmarketcap.com')
+					}
+				},
+				{ type: 'separator' },
+				{ 
+					label: 'exit',
+					click() {
+						console.log('this app will be close')
+						app.quit()
+					}
+				}
+			]
+		},
+		{
+			label: 'Favorites',
+			submenu: [
+				{ label: 'Pizza' },
+				{ label: 'Chiken' },
+				{ label: 'Fish' }
+			]
+		}
+	])
+
+	Menu.setApplicationMenu(menu);
+}
+
+// This method will be called when the elecetron has finished
+// initialization and is ready to create browser windows
+// some APIS can only be used after this event occurs
+app.on('ready', createWindow)
+
+// Quit when all window are closed
+app.on('window-all-closed', () => {
+	// on macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicity with cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
+
+app.on('activate', () => {
+	// on macOS it's common to re-create a window in the app when the dock icon is clicked and there are no other windows open
+
+	if (win == null) {
+		createWindow()
+	}
+})
+
+// In this file you can include the rest of the app's specific main process code.
+// You can also put them in separate files and require then here.
